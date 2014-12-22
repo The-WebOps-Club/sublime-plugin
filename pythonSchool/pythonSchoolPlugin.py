7729077e4aeddff7a92d44ab2f5f07cca370f900
vignesh-m <vicky.mano@gmail.com>
2014-12-22 13:22:47 +0530

    fixed st3 bugs

--- pythonSchool/pythonSchoolPlugin.py
+++ pythonSchool/pythonSchoolPlugin.py
@@ -4,7 +4,9 @@ class PythonSchoolCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		path=sublime.packages_path()+"datapythonschool/1.py"
 		self.create(path)
-		self.window.active_view().run_command('open_file_insert',{'path':path,'text':'Hello\n'})
+		self.window.active_view().run_command('open_file_insert',{'path':path,'text':'print ("Hello\\n\")'})
+		view_curr=self.window.active_view()
+		view_curr.run_command('save')
 
 	def create(self, filename):
 		base, filename = os.path.split(filename)
@@ -15,21 +17,32 @@ class PythonSchoolCommand(sublime_plugin.WindowCommand):
 			parent = os.path.split(base)[0]
 			if not os.path.exists(parent):
 				self.create_folder(parent)
-			os.mkdir(base)
+			try :
+				os.mkdir(base)
+			except :
+				print ('Error while opening file');
 
 class CheckOutputCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		fname=self.view.file_name();
 		proc = subprocess.Popen(['python', fname], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		output= proc.communicate()[0]
+		new_view=self.view.window().new_file()
+		new_view.run_command('goto_line',{'line':1});
+		pos=new_view.sel()[0].begin();
+		new_view.insert(edit,pos,"Your Output:\n\n"+(output.decode('utf-8')));
 		print (output)
 
 class OpenFileInsertCommand(sublime_plugin.TextCommand):
     def run(self,edit,path,text):
         window = self.view.window()
-        #path='/Users/vigneshm/Desktop/st_plugin/1.py'
-        view = window.open_file(path)
+        if(os.path.exists(path)):
+        	view = window.open_file(path)
+        else :
+        	print('Creating ...')
+        	view = window.open_file(path)
         sublime.set_timeout(lambda: self.select_text(view,edit,text), 10)
+
     def select_text(self, view,edit,text):
         if not view.is_loading():
             view.run_command('insert_text',{'pos':0,'text':text})
