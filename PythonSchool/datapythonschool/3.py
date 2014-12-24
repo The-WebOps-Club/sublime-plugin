804951f2ff232e4e75a4b0651d30607fc7ea00a7
vignesh-m <vicky.mano@gmail.com>
2014-12-24 17:35:59 +0530

    added prob 3

---
+++ PythonSchool/datapythonschool/3.py
@@ -0,0 +1,16 @@
+# Variables in python are very similar to variable in algebra - they can have 
+# various values which you can use without actually knowing the value.
+
+# Creating (Declaring) a variable is as simple as  a = 1 which assigns the 
+# value of 1 to the variable a.In this case , 'a' is an 'int' (Integer) 
+# variable which you can check by executing type(a) 
+
+# Variables can be treated like values , as long as the performed operation 
+# exists . For example, you can do a+3 (a is int) but not name-"po" (name
+# is a str / string)
+
+# Your task is to set a variable v to 2.5 and print its type.Also print its 
+# square.Create another variable animal equal to the string Python and find 
+# what animal*3 does and print it.
+
+print "foofoofoo"
---
+++ PythonSchool/datapythonschool/3soln.py
@@ -0,0 +1,5 @@
+v=2.5
+print (type(v))
+print (v*v)
+animal="foo"
+print (animal*3)
