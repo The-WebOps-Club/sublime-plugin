41ca0f1878d73b20927466504f255b78d68b9cd5
Kailash-K <kailashsk631@gmail.com>
2014-12-28 00:54:41 +0530

    added another 3 pblms

---
+++ PythonSchool/datapythonschool/8out.py
@@ -0,0 +1,7 @@
+#### OUTPUT START ####
+family = {'father':'mike','mom':'lilly','bro':'brad','sis':'reena'}
+print(family['bro'])
+print(family['sis'])
+ages = {'father':44,'mom':39,'bro':24,'sis':19}
+print('kite' in ages)
+#### OUTPUT END ####
