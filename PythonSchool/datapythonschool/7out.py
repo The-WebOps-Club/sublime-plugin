4013ae639b121502e872c1c1ef6c7753fc852ad5
Kailash-K <kailashsk631@gmail.com>
2014-12-29 12:24:36 +0530

    updated prblm 7

--- PythonSchool/datapythonschool/7out.py
+++ PythonSchool/datapythonschool/7out.py
@@ -4,4 +4,7 @@ extra =['pineapple','custard apple','cherry']
 print(grocery[2])
 print(extra[0])
 print(grocery + extra)
+print(grocery[2:4])
+print(extra[-1:-3:-1])
+print(grocery[1::2])
 #### OUTPUT END ####
